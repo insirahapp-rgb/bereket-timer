65d74c9deaf3ef64a1c4c81dd2b7371f65e2af73
insirahapp-rgb <insirahapp@gmail.com>
2025-12-02 21:55:25 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,43 +1,15 @@
 const CACHE_NAME = 'bereket-timer-v1';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/manifest.json',
-    '/icon-512.png'
-];
 
-// Install
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-    );
+    self.skipWaiting();
 });
 
-// Fetch
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
+        fetch(event.request).catch(() => caches.match(event.request))
     );
 });
 
-// Activate
 self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+    event.waitUntil(clients.claim());
 });
